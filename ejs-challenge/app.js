608e0618e1f9562d46d70aefa3589fc299bfd06a
Terry Dengis <Terry.Dengis@icloud.com>
2019-12-19 11:35:23 -0700

    Add persitence with MongoDB

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -4,8 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const _ = require('lodash');
+const mongoose = require('mongoose');
 
-const posts = [];
 const homeStartingContent =
   'Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.';
 const aboutContent =
@@ -20,8 +20,26 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+mongoose.connect('mongodb://localhost:27017/blogDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const postSchema = {
+  title: String,
+  body: String
+};
+
+const Post = mongoose.model('Post', postSchema);
+
 app.get('/', (req, res) => {
-  res.render('home', { homeContent: homeStartingContent, posts: posts });
+  Post.find({}, (err, posts) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('home', { homeContent: homeStartingContent, posts: posts });
+    }
+  });
 });
 
 app.get('/about', (req, res) => {
@@ -37,20 +55,23 @@ app.get('/compose', (req, res) => {
 });
 
 app.post('/compose', (req, res) => {
-  const post = {
+  const post = new Post({
     title: req.body.postTitle,
     body: req.body.postDetail
-  };
-  posts.push(post);
+  });
+  post.save();
   res.redirect('/');
 });
 
 app.get('/posts/:postId', (req, res) => {
   const requestedTitle = req.params.postId;
-  const found = posts.find(post => _.lowerCase(post.title) === _.lowerCase(requestedTitle));
-  if (found) {
-    res.render('post', { post: found });
-  }
+  Post.findOne({ _id: requestedTitle }, (err, post) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('post', { post: post });
+    }
+  });
 });
 
 app.listen(3000, function() {
