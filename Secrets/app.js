36e7e657e49019b64f80e16f58db73352050d3c3
Terry Dengis <Terry.Dengis@icloud.com>
2019-12-23 14:45:06 -0700

    Level 2 encryption

---
+++ Secrets/app.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+mongoose.connect('mongodb://localhost:27017/userDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+const secret = 'Thisisoutlittlesecret';
+userSchema.plugin(encrypt, { secret, encryptedFields: ['password'] });
+
+const User = new mongoose.model('User', userSchema);
+
+// TODO: Enter Your Code here
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+app.post('/register', (req, res) => {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+
+  newUser.save(err => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.render('secrets');
+    }
+  });
+});
+
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  User.findOne({ email: username }, (err, result) => {
+    if (err) {
+      res.send(err);
+    } else {
+      if (result) {
+        if (result.password === password) {
+          res.render('secrets');
+        }
+      }
+    }
+  });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server running on port 3000');
+});
